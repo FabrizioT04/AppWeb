153e7f59af4bc71487204158c17d7b0756989a46
Fabrizio Tejeda <fabriziotp0001@gmail.com>
2025-10-01 17:19:20 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'InstitucionEducativa2005',
+  webDir: 'www'
+};
+
+export default config;
